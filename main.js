1ff39526b2197ef54201edcb379b6dafa405d87f
Lewait <alfaro0297@gmail.com>
2023-06-22 23:58:50 -0500

    add: creaciòn del header

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import './main.css'
+import './styles/main.css'
+
+const getArtist = async (id) => {
+  const req = await window.fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  const res = await req.json()
+  console.log(res)
+}
+getArtist('ditto')
